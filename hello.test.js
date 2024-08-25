56290a2e911f17446aa5c80c1fffa43d50a1425f
sdhiman <sudeep_dhiman@intuit.com>
2024-08-25 15:32:38 -0500

    update hello-test

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Office Hours in the terminal!");
+    expect(hello.hello()).toEqual("Hello World from live music capital of the word: Austin, TX");
   });
 });
